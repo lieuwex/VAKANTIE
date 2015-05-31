646bb4b07bb8c4ac07329f7460c8052eb95982a0
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2015-05-31 21:55:20 +0200

    More precise friendlyDates.

---
+++ friendly.js
@@ -0,0 +1,61 @@
+function _count (count) {
+	return [
+		"nul",
+		"één",
+		"twee",
+		"drie",
+		"vier",
+		"vijf",
+		"zes",
+		"zeven",
+		"acht",
+		"negen",
+		"tien",
+		"elf",
+		"twaalf",
+		"dertien",
+		"viertien",
+		"vijftien",
+		"zestien",
+		"zeventien",
+		"achtien",
+		"negentien",
+		"twintig"
+	][count] || count.toString();
+}
+function _pluralize (amount, words) {
+	var amount = ~~amount;
+	return _count(amount) + " " + words[+(amount !== 1)];
+}
+function months (months) { return _pluralize(months, ["maand", "maanden"]);  }
+function weeks (weeks) { return _pluralize(weeks, ["week", "weken"]);  }
+function days (days) { return _pluralize(days, ["dag", "dagen"]);  }
+
+module.exports = function (duration) {
+	var leftDays = duration.asDays();
+	var leftWeeks = leftDays / 7;
+	var leftMonths = duration.asMonths();
+
+	if (leftMonths > 2) {
+		return "ongeveer " + months(leftMonths) + ".";
+	} else if (leftMonths > 1) {
+		var s = months(leftMonths);
+		var leftWeeks = leftWeeks % 4;
+
+		if (leftWeeks === 0) {
+			return s;
+		} else if (~~leftWeeks === 0) {
+			return "ongeveer " + s;
+		} else if (leftWeeks%1 === 0) {
+			return s + " en " + weeks(leftWeeks % 4) + ".";
+		} else {
+			return s + " en ongeveer " + weeks(leftWeeks % 4) + ".";
+		}
+	} else {
+		if (leftDays % 7 === 0) {
+			return weeks(leftWeeks) + ".";
+		} else {
+			return weeks(leftWeeks) + " en " + days(leftDays % 7) + ".";
+		}
+	}
+};
